4d78e322b38ab92245dd0aea79556d28329c6b38
shaquib khan <connectshaquib@gmail.com>
2022-12-08 22:53:11 +0530

    created rapid api card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+// http 
+import {HttpClientModule} from '@angular/common/http'
+import { ToastrModule } from 'ngx-toastr';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome'
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ToastrModule.forRoot({
+      positionClass :'toast-bottom-right'
+    }),
+    BrowserAnimationsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+
+
+import {
+  faEnvelope,
+  faMapMarkedAlt,
+  faPhone,
+  faDatabase
+} from '@fortawesome/free-solid-svg-icons'
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+
+ 
+export class CardComponent {
+
+  @Input() user: any;
+  faEnvelope= faEnvelope;
+  faMapMarkedAlt = faMapMarkedAlt;
+  faPhone = faPhone;
+  faDatabase = faDatabase;
+  
+}
